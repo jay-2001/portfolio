590f5586508b64188bb4a10dcf2effa5a98ff4bb
jay-2001 <90552695+jay-2001@users.noreply.github.com>
2024-11-23 22:33:19 +0530

    Portfolio

---
+++ src/components/constants.js
@@ -0,0 +1,38 @@
+export const LINKS = {
+    LINKEDIN_URL: "https://www.linkedin.com/in/rohitk06/",
+    INSTAGRAM_URL: "https://www.instagram.com/jay_n_patel__/",
+    GITHUB_URL: "https://github.com/jay-2001"
+}
+
+export const DETAILS = {
+    NAME: "Jay",
+    FULL_NAME: "Jay Patel",
+    EMAIL: "jaynpatel222@gmail.com",
+    FOOTER_MESSAGE: "A Frontend focused Web Developer building the Frontend of Websites and\
+          Web Applications that leads to the success of the overall product"
+}
+
+export const ABOUT = {
+    ROLE: "FullStack Web Developer",
+    DESC: "building the Front-end of Websites and Web Applications that leads to\
+        the success of the overall product. Check out some of my work in the\
+        Projects section.I also like sharing content related to the stuff that I\
+        have learned over the years in Web Development so it can help other\
+        people of the Dev Community. Feel free to Connect or Follow me on my"
+}
+
+export const JOURNEY = {
+    HEADING: "This section tells about my life Journey in tech and education field",
+    JOB1: {
+        ROLE: "Software Developememt Engineer I",
+        TYPE: "Jan 2024 - Present",
+        EMPLOYEE: "GeeksforGeeks",
+        LINK: ""
+    },
+    JOB2: {
+        ROLE: "Software Developer",
+        TYPE: "Jan 2023 - Dec 2023",
+        EMPLOYEE: "Josh Technology Group",
+        LINK: ""
+    }
+}
